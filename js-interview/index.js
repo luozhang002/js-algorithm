82a3d75e2fc2e5f134b85a46e9be7e650448335c
罗章 <xiaoluo@meili-inc.com>
2018-03-18 21:25:37 +0800

    提交demo

--- js-interview/index.js
+++ js-interview/index.js
@@ -120,5 +120,17 @@ function random(n){
   return tmp;
 }
 
-
-
+//深入拷贝
+function deepClone(obj) {
+    var newObj = obj instanceof Array ? [] : {};
+    //obj属于基本数据类型,直接返回obj
+    if(typeof obj !== 'object') {
+        return obj;
+    } else {
+    //obj属于数组或对象，遍历它们
+        for(var i in obj) {
+          newObj[i] = typeof obj[i] === 'object' ? deepClone(obj[i]):obj[i];
+        }
+    }
+    return newObj;
+}
---
+++ js-interview/linklist/index.js
@@ -0,0 +1,11 @@
+//单链表的创建和遍历
+
+function LinkList () {
+  var length = 0;
+  var head = null;
+
+  var Node = function (data,next) {
+    this.data = data;
+    this.next = null;
+  }
+}
